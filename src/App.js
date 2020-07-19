51474d98c5d4139e29246a041f94127be9c79947
Chang <cnxiong03@gmail.com>
2020-07-19 18:35:47 -0400

    first part of starting our react app passing default data to our app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// useState enables us to store state in our function component
+import React, {useState} from 'react';
+import PokemonList from './PokemonList';
 
 function App() {
+  // pokemon will be our actual data while setPokemon will be the way which allows
+  // us to update the list when we go to another page
+  
+  // to see how our app will look like we will add some default state of two different pokemon
+  const [pokemon, setPokemon] = useState(['bulbasaur', 'charmander']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // we want to render PokemonList and pass in the props which is pokemon & it neeeds a list of 
+    // all the the pokemon to render so now we are passing our pokemon down to our PokemonList
+    <PokemonList pokemon={pokemon} />
   );
 }
 
